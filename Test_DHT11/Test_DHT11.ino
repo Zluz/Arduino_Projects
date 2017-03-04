4bd1efd7d62bfdbec4741aee2e4e4f6a3039b897
Administrator <Administrator@vm101.home>
2017-03-04 15:13:00 -0500

    Adding program to test DHT devices

---
+++ Test_DHT11/Test_DHT11.ino
@@ -0,0 +1,30 @@
+
+// see also
+// https://github.com/adafruit/DHT-sensor-library/blob/master/examples/DHTtester/DHTtester.ino
+
+#include "DHT.h"
+
+#define PIN 10
+DHT dht( PIN, DHT11 );
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  dht.begin();
+}
+
+
+void loop() {
+
+  const float fHumid = dht.readHumidity();
+  const String strHumid = String(fHumid);
+  Serial.println( "fHumid = " + strHumid );
+
+  const float fTemp = dht.readTemperature(true);
+  const String strTemp = String(fTemp);
+  Serial.println( "fTemp = " + strTemp );
+
+  delay(2000);
+}
+
